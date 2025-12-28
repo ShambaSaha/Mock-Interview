06e2d1b49619463afc5e029052fb6b296152694f
ShambaSaha <shambasaha@gmail.com>
2025-12-28 13:22:41 +0530

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+import AuthForm from '@/app/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AuthForm type="sign-in" />
+    </div>
+  )
+}
+
+export default page
+
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,13 @@
+import AuthForm from '@/app/components/AuthForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <AuthForm type="sign-up" />
+    </div>
+  )
+}
+
+export default page
+
