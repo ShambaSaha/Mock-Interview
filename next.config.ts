d001fafa926d568b4e347cd6f44c44629941e962
Preyoshi04 <preyoshimondal@gmail.com>
2026-01-02 16:41:30 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  typescript: {
+  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript:{
     ignoreBuildErrors: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        net: false,
-        tls: false,
-        child_process: false,
-      };
-    }
-    return config;
-  },
+  }
 };
 
 export default nextConfig;
