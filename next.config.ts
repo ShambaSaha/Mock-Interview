289b063632eb11d1924cf43329bf80a5737b6c81
Preyoshi04 <preyoshimondal@gmail.com>
2026-01-02 14:56:50 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,24 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript:{
+//     ignoreBuildErrors: true,
+//   }
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true, // This allows the build even with TS errors
   },
-  typescript:{
-    ignoreBuildErrors: true,
-  }
+  eslint: {
+    ignoreDuringBuilds: true, // This bypasses the eslint error in your log
+  },
 };
 
+module.exports = nextConfig;
 export default nextConfig;
